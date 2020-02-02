41448f772607929f88505004fb8bbba80ef06d79
AxlEstevez <lacura4ever@gmail.com>
2020-02-02 17:01:40 -0600

    Se han agregado las primeras funciones en el main.js. Las funciones por el momento solo muestran o esconden elementos. Po ultimo se agrego un div en donde se pretende sera el tablero de juego

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function comenzar(){
+    var esconder = document.getElementById("contedor-menu");
+    var tablero = document.getElementById("tablero");
+    esconder.style.display = "none";
+    tablero.style.display = "block";
+
+}
+
+function menu() {
+    var esconder = document.getElementById("contedor-menu");
+    var tablero = document.getElementById("tablero");
+    esconder.style.display = "block";
+    tablero.style.display = "none";
+}
