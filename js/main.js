21dba4e01f3734516304894cc901725cd4429696
AxlEstevez <lacura4ever@gmail.com>
2020-02-03 14:15:29 -0600

    Se ha modificado la función de comenzar, ahora recibe dos parametros para detectar que continente a jugar y el número de banderas a desplegar. Se agredo una tercera función la cual va a mandar al html banderas correspondientes al continente seleccionado y y el número de banderas-

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-function comenzar(){
+function comenzar(continente, cantidad){
+    var continent = document.getElementById(continente).value;
+    var banderas = document.getElementById(cantidad).value;
     var esconder = document.getElementById("contedor-menu");
     var tablero = document.getElementById("tablero");
     esconder.style.display = "none";
     tablero.style.display = "block";
+    comenzarJuego(continent,banderas);
 
 }
 
@@ -11,4 +14,43 @@ function menu() {
     var tablero = document.getElementById("tablero");
     esconder.style.display = "block";
     tablero.style.display = "none";
+}
+
+function comenzarJuego(continente,noBanderas) {
+    switch (continente) {
+        case "America":
+            // Código aquí
+            // --------------------------------------------
+            // código de prueba.
+            alert("se despliega " +  noBanderas + " banderas de America");
+            break;
+        case "Asia":
+            // Código aquí
+            // --------------------------------------------
+            // código de prueba.
+            alert("se despliega " +  noBanderas + " banderas de Asia");
+            break;
+        case "Africa":
+            // Código aquí
+            // --------------------------------------------
+            // código de prueba.
+            alert("se despliega " +  noBanderas + " banderas de Africa");
+            break;
+        case "Europa":
+            // Código aquí
+            // --------------------------------------------
+            // código de prueba.
+            alert("se despliega " +  noBanderas + " banderas de Europa");
+            break;
+        case "Oceania":
+            // Código aquí
+            // --------------------------------------------
+            // código de prueba.
+            alert("se despliega " +  noBanderas + " banderas de Oceania");
+            break;
+        default:
+            alert("Selecciona una de las opciones");
+            
+            break;
+    }
 }
