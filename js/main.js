bfce8aa71a365c60110251dcdc4fcdfec7fff5ef
AxlEstevez <lacura4ever@gmail.com>
2020-02-09 21:30:40 -0600

    Se ha agregado nuevas funcionalidades al sistema, se ha comentado todo el codigo. El sistema ahora es capaz de cargar las imagenes dinamicamente a un avariable de tipo arreglo se despliegan las imagenes de acuerdo al numero de banderas elegido por el usuario PROBLEMAS A RESOLVER. Solucionar la repeticion de los numeros aleatorios, verificar la agregacion completa de los objetos imagen al tablero ya que en ocaciones agrega todas las imagenes y en ocaciones hay faltantes.

--- js/main.js
+++ js/main.js
@@ -7,32 +7,147 @@
 //   <axlestevez@hotmail.com>
 // --------------------------------------------------------
 
+// --------------------------------------------------------
+// Constantes
+var botonRegresar = document.createElement('input');
+// --------------------------------------------------------
 
-function comenzar(continente, cantidad){
+// --------------------------------------------------------
+// Las variables total representan el total de imagenes
+// contenidas por carpeta.
+// --------------------------------------------------------
+var totalPaisesAmerica = 45;
+var totalPaisesAsia = 51;
+var totalPaisesAfrica = 59;
+var totalPaisesEuropa = 58;
+var totalPaisesOceania = 20;
+// --------------------------------------------------------
+// Variables que tendran cargadas las imagenes por
+// continente.
+// --------------------------------------------------------
+var america = [];
+var asia = [];
+var africa = [];
+var europa = [];
+var oceania = [];
+// --------------------------------------------------------
+// Las siguientes variables contendran los nombres de los
+// países por cada continente.
+// --------------------------------------------------------
+var paisesAmerica = ['puerto rico','nicaragua','salvador',
+    'republica dominicana','hawaii','jamaica','honduras',
+    'uruguay','guatemala','panama','ecuador','chile',
+    'venezuela','colombia','argentina','peru','anguilla',
+    'islas caiman','aruba','antigua y barbuda','bonaire',
+    'barbados','bermuda','belice','cuba','costa rica',
+    'bolivia','islas galapagos','martinica','santa lucia',
+    'dominica','estado unidos','san vicente y las granadias',
+    'san eustaquio','san cristobal y nieves','granada',
+    'groelandia','bahamas','surinam','canada','curazao',
+    'trinidad y tobago','mexico','haiti','brasil'];
+var paisesAsia = [];
+var paisesAfrica = [];
+var paisesEuropa = [];
+var paisesOceania = [];
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Métodos / funciones
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Método/función : inicializaAmerica.
+// Parámetros: Ninguno.
+// Retorna: Nada.
+// Objetivo: Llenar el array america con imagenes.
+// --------------------------------------------------------
+function inicializaAmerica() {
+    var texto;
+
+    for(var i = 0; i < totalPaisesAmerica; i++){
+        texto = ('images/America/' + i + ".png");
+        america[i] = cargaImagen(texto);
+    }
+}
+// fin de Método/función.
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Método/función: cargaImagen.
+// Parámetros: nombre, recibe el nombre o ruta de la imagen
+// que se pretende cargar.
+// Retorna: Un objeto Image.
+// Objetivo: Crea un objeto de tipo Image(imagen) con el
+// fin de poder manipular ciertos atributos.
+// --------------------------------------------------------
+function cargaImagen(nombre) {
+    var imagen = new Image();
+    imagen.src = nombre;
+    imagen.className = "icon-tablero";
+
+    return imagen;
+}
+// Fin de Método/función.
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Método/función: cargaJuego.
+// Parámetros: noBanderas, arreglo, tablero
+// Retorna: -
+// Objetivo: obtener un número finito de objetos de manera 
+// aleatoria de un arreglo dado.
+// --------------------------------------------------------
+function cargaBanderas(noBanderas, array,tablero) {
+    var i = 0;
+    while(i < noBanderas){
+        var posicion = Math.floor(Math.random()*totalPaisesAmerica);
+        tablero.appendChild(array[posicion]);
+        console.log(posicion + "" + array[posicion].src);
+        i++;
+    }
+    botonRegresar.setAttribute('type','button');
+    botonRegresar.setAttribute('value','Regresar');
+    botonRegresar.setAttribute('onclick','menu();');
+    tablero.appendChild(botonRegresar);
+}
+// fin Método / Función
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Método/función: start.
+// Parámetros: continente, cantidad
+// Retorna: nada
+// Objetivo: Inicia el juego. Manda a llamar a todas las 
+// funciones necesarias para poder jugar el juego.
+// --------------------------------------------------------
+function start(continente, cantidad){
     var continent = document.getElementById(continente).value;
     var banderas = document.getElementById(cantidad).value;
     var esconder = document.getElementById("contedor-menu");
     var tablero = document.getElementById("tablero");
+    var box = document.getElementById("box");
+    box.style.display = "block";
     esconder.style.display = "none";
     tablero.style.display = "block";
-    comenzarJuego(continent,banderas);
-
+    menuJuego(continent,banderas);
+   
 }
+// fin Método / Función
+// --------------------------------------------------------
 
-function menu() {
-    var esconder = document.getElementById("contedor-menu");
+// --------------------------------------------------------
+// Método/función: menuJuego.
+// Parámetros: continente, noBanderas
+// Retorna: nada
+// Objetivo: Verifica que es lo que el usuario pretende
+// jugar y de acuerdo a eso lanza lo necesario.
+// --------------------------------------------------------
+function menuJuego(continente,noBanderas) {
     var tablero = document.getElementById("tablero");
-    esconder.style.display = "block";
-    tablero.style.display = "none";
-}
-
-function comenzarJuego(continente,noBanderas) {
     switch (continente) {
         case "America":
-            // Código aquí
-            // --------------------------------------------
-            // código de prueba.
-            alert("se despliega " +  noBanderas + " banderas de America");
+            inicializaAmerica();
+            cargaBanderas(noBanderas,america,tablero);
             break;
         case "Asia":
             // Código aquí
@@ -64,3 +179,26 @@ function comenzarJuego(continente,noBanderas) {
             break;
     }
 }
+// fin Método / Función
+// --------------------------------------------------------
+
+// --------------------------------------------------------
+// Método/función: menu.
+// Parámetros: ninguno
+// Retorna: nada
+// Objetivo: Es una fución auxiliar para ocultar o mostrar
+// elementos en el html.
+// --------------------------------------------------------
+function menu() {
+    var esconder = document.getElementById("contedor-menu");
+    var tablero = document.getElementById("tablero");
+    var box = document.getElementById("box");
+    esconder.style.display = "block";
+    tablero.style.display = "none";
+    box.style.display = "none"; 
+}
+// fin Método / Función
+// --------------------------------------------------------
+
+// fin del archivo.
+// --------------------------------------------------------
